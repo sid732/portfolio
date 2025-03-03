e18bec0f2d19047bbf563776184dbf5d9b31e8fb
SIDDHARTH YOGESH NASHIKKAR <nashikkar.s@northeastern.edu>
2025-03-02 23:03:38 -0500

    removed background effect

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,10 @@ import HomePage from './pages/Home';
 // import Projects from './pages/Projects';
 // import Skills from './pages/Skills';
 // import Contact from './pages/Contact';
-import BackgroundEffect from './components/layout/BackgroundEffect';
-
 function App() {
   return (
     <Router>
       <div className="w-full min-h-screen bg-[#1a1625] relative">
-        <BackgroundEffect />
         <div className="h-full overflow-y-auto">
           <AnimatePresence mode='wait'>
             <Routes>
